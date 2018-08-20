764a7a63000ee778027deb6ea189718f87f893cd
Ken Chan <kenchanhp@gmail.com>
2018-08-20 12:27:07 +0800

    Log the computation time in when doing inference on the validation set.

--- evaluate.py
+++ evaluate.py
@@ -4,14 +4,18 @@ import torch
 #from pykp.metric.bleu import bleu
 from pykp.masked_loss import masked_cross_entropy
 from utils.statistics import Statistics
+import time
+from utils.time_log import time_since
 
 def evaluate_loss(data_loader, model, opt):
     model.eval()
     evaluation_loss_sum = 0.0
     total_trg_tokens = 0
+    total_batch = 0
 
     with torch.no_grad():
         for i, batch in enumerate(data_loader):
+            total_batch += 1
             src, src_lens, src_mask, trg, trg_lens, trg_mask, src_oov, trg_oov, oov_lists = batch
 
             max_num_oov = max([len(oov) for oov in oov_lists])  # max number of oov for each batch
@@ -24,18 +28,22 @@ def evaluate_loss(data_loader, model, opt):
             src_oov = src_oov.to(opt.device)
             trg_oov = trg_oov.to(opt.device)
 
+            start_time = time.time()
             decoder_dist, h_t, attention_dist, coverage = model(src, src_lens, trg, src_oov, max_num_oov, src_mask)
+            forward_time = time_since(start_time)
 
+            start_time = time.time()
             if opt.copy_attention:  # Compute the loss using target with oov words
                 loss = masked_cross_entropy(decoder_dist, trg_oov, trg_mask, trg_lens,
                                             opt.coverage_attn, coverage, attention_dist, opt.lambda_coverage)
             else:  # Compute the loss using target without oov words
                 loss = masked_cross_entropy(decoder_dist, trg, trg_mask, trg_lens,
                                             opt.coverage_attn, coverage, attention_dist, opt.lambda_coverage)
+            loss_compute_time = time_since(start_time)
 
             evaluation_loss_sum += loss.item()
             total_trg_tokens += sum(trg_lens)
 
-    eval_loss_stat = Statistics(evaluation_loss_sum, total_trg_tokens)
+    eval_loss_stat = Statistics(evaluation_loss_sum, total_trg_tokens, total_batch, forward_time=forward_time, loss_compute_time=loss_compute_time)
 
     return eval_loss_stat
