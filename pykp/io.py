Log the computation time for several operations. Update the scripts for plotting train-valid-curve and output the corresponding data to a csv. Update the Statistics class. Fix the bug of cross-entropy loss by adding a small offset before doing torch.log to the probabilities.

pykp/io.py
@@ -12,38 +12,19 @@ from collections import Counter
 from collections import defaultdict
 import numpy as np
 import sys
-from torch.autograd import Variable
-
-import torch.multiprocessing as multiprocessing
-import queue
-import threading
-
 
 __author__ = "Rui Meng"
 __email__ = "rui.meng@pitt.edu"
 
-import torchtext
+#import torchtext
 import torch
+import torch.utils.data
 
 PAD_WORD = '<pad>'
 UNK_WORD = '<unk>'
 BOS_WORD = '<bos>'
 EOS_WORD = '<eos>'
 DIGIT = '<digit>'
-
-
-def __getstate__(self):
-    return dict(self.__dict__, stoi=dict(self.stoi))
-
-
-def __setstate__(self, state):
-    self.__dict__.update(state)
-    self.stoi = defaultdict(lambda: 0, self.stoi)
-
-
-torchtext.vocab.Vocab.__getstate__ = __getstate__
-torchtext.vocab.Vocab.__setstate__ = __setstate__
-
 
 class KeyphraseDataset(torch.utils.data.Dataset):
     def __init__(self, examples, word2idx, idx2word, type='one2many', include_original=False):
@@ -204,7 +185,7 @@ class KeyphraseDataset(torch.utils.data.Dataset):
         else:
             return (src_o2m, src_o2m_len, trg_o2m, None, trg_copy_target_o2m, src_oov_o2m, oov_lists_o2m), (src_o2o, src_o2o_len, trg_o2o, trg_target_o2o, trg_copy_target_o2o, src_oov_o2o, oov_lists_o2o)
 
-
+'''
 class KeyphraseDatasetTorchText(torchtext.data.Dataset):
     @staticmethod
     def sort_key(ex):
@@ -226,7 +207,7 @@ class KeyphraseDatasetTorchText(torchtext.data.Dataset):
                 [src_tokens, trg_tokens], fields))
 
         super(KeyphraseDatasetTorchText, self).__init__(examples, fields, **kwargs)
-
+'''
 
 def load_json_data(path, name='kp20k', src_fields=['title', 'abstract'], trg_fields=['keyword'], trg_delimiter=';'):
     '''
@@ -554,7 +535,7 @@ def copy_martix(source, target):
                 cc[i][j] = 1.
     return cc
 
-
+'''
 def build_vocab(tokenized_src_trgs_pairs, opt):
     """Construct a vocabulary from tokenized lines."""
     vocab = {}
@@ -567,26 +548,26 @@ def build_vocab(tokenized_src_trgs_pairs, opt):
                 vocab[token] += 1
 
     # Discard start, end, pad and unk tokens if already present
-    if '<s>' in vocab:
-        del vocab['<s>']
+    if '<bos>' in vocab:
+        del vocab['<bos>']
     if '<pad>' in vocab:
         del vocab['<pad>']
-    if '</s>' in vocab:
-        del vocab['</s>']
+    if '<eos>' in vocab:
+        del vocab['<eos>']
     if '<unk>' in vocab:
         del vocab['<unk>']
 
     word2idx = {
         '<pad>': 0,
-        '<s>': 1,
-        '</s>': 2,
+        '<bos>': 1,
+        '<eos>': 2,
         '<unk>': 3,
     }
 
     idx2word = {
         0: '<pad>',
-        1: '<s>',
-        2: '</s>',
+        1: '<bos>',
+        2: '<eos>',
         3: '<unk>',
     }
 
@@ -605,102 +586,9 @@ def build_vocab(tokenized_src_trgs_pairs, opt):
         idx2word[ind + 4] = word
 
     return word2idx, idx2word, vocab
+'''
 
-
-class One2OneKPDatasetOpenNMT(torchtext.data.Dataset):
-    def __init__(self, src_trgs_pairs, fields,
-                 src_seq_length=0, trg_seq_length=0,
-                 src_seq_length_trunc=0, trg_seq_length_trunc=0,
-                 dynamic_dict=True, **kwargs):
-
-        self.src_vocabs = []
-        # examples: one for each src line or (src, trg) line pair.
-        # Each element is a dictionary whose keys represent at minimum
-        # the src tokens and their indices and potentially also the
-        # src and trg features and alignment information.
-        examples = []
-        indices = 0
-
-        for src, trgs in src_trgs_pairs:
-            if src_seq_length_trunc > 0 and len(src) > src_seq_length_trunc:
-                src = src[:src_seq_length_trunc]
-            for trg in trgs:
-                trg = re.sub('\(.*?\)', '', trg).strip()
-                if trg_seq_length_trunc > 0 and len(trg) > trg_seq_length_trunc:
-                    trg = trg[:trg_seq_length_trunc]
-                examples.append({'indices': indices, 'src': src, 'trg': trg})
-                indices += 1
-
-        if dynamic_dict:
-            examples = self.dynamic_dict(examples)
-
-        keys = fields.keys()
-        fields = [(k, fields[k]) for k in keys]
-        example_values = ([ex[k] for k in keys] for ex in examples)
-
-        # internally call the field.preprocess() and process each scr and trg
-        #       including lower(), tokenize() and preprocess()
-        out_examples = (torchtext.data.Example.fromlist(ex_values, fields)
-                        for ex_values in example_values)
-
-        def filter_pred(example):
-            return 0 < len(example.src) <= src_seq_length \
-                and 0 < len(example.trg) <= trg_seq_length
-
-        super(One2OneKPDatasetOpenNMT, self).__init__(
-            out_examples,
-            fields,
-            filter_pred
-        )
-
-    def dynamic_dict(self, examples):
-        for example in examples:
-            src = example["src"]
-            src_vocab = torchtext.vocab.Vocab(Counter(src))
-            self.src_vocabs.append(src_vocab)
-            # mapping source tokens to indices in the dynamic dict
-            src_map = torch.LongTensor([src_vocab.stoi[w] for w in src])
-            example["src_map"] = src_map
-
-            if "trg" in example:
-                trg = example["trg"]
-                mask = torch.LongTensor(
-                    [0] + [src_vocab.stoi[w] for w in trg] + [0])
-                example["alignment"] = mask
-            yield example
-
-    @staticmethod
-    def sort_key(ex):
-        "Sort in reverse size order"
-        return -len(ex.src)
-
-    def __getstate__(self):
-        return self.__dict__
-
-    def __setstate__(self, d):
-        self.__dict__.update(d)
-
-    def __reduce_ex__(self, proto):
-        "This is a hack. Something is broken with torch pickle."
-        return super(One2OneKPDatasetOpenNMT, self).__reduce_ex__()
-
-
-def merge_vocabs(vocabs, vocab_size=None):
-    """
-    Merge individual vocabularies (assumed to be generated from disjoint
-    documents) into a larger vocabulary.
-    Args:
-        vocabs: `torchtext.vocab.Vocab` vocabularies to be merged
-        vocab_size: `int` the final vocabulary size. `None` for no limit.
-    Return:
-        `torchtext.vocab.Vocab`
-    """
-    merged = sum([vocab.freqs for vocab in vocabs], Counter())
-    return torchtext.vocab.Vocab(merged,
-                                 specials=[PAD_WORD, BOS_WORD, EOS_WORD],
-                                 max_size=vocab_size)
-
-
+'''
 def save_vocab(fields):
     vocab = []
     for k, f in fields.items():
@@ -708,38 +596,4 @@ def save_vocab(fields):
             f.vocab.stoi = dict(f.vocab.stoi)
             vocab.append((k, f.vocab))
     return vocab
-
-
-def build_vocab_OpenNMT(train, opt):
-    """
-    train: a KPDataset
-    """
-    fields = train.fields
-    fields["src"].build_vocab(train, max_size=opt.vocab_size,
-                              min_freq=opt.words_min_frequency)
-    fields["trg"].build_vocab(train, max_size=opt.vocab_size,
-                              min_freq=opt.words_min_frequency)
-    merged_vocab = merge_vocabs(
-        [fields["src"].vocab, fields["trg"].vocab],
-        vocab_size=opt.vocab_size)
-    fields["src"].vocab = merged_vocab
-    fields["trg"].vocab = merged_vocab
-
-
-def initialize_fields(opt):
-    """
-    returns: A dictionary whose keys are strings and whose values are the
-            corresponding Field objects.
-    """
-    fields = {}
-    fields["src"] = torchtext.data.Field(
-        init_token=BOS_WORD, eos_token=EOS_WORD,
-        pad_token=PAD_WORD, lower=opt.lower,
-        tokenize=copyseq_tokenize)
-
-    fields["trg"] = torchtext.data.Field(
-        init_token=BOS_WORD, eos_token=EOS_WORD,
-        pad_token=PAD_WORD, lower=opt.lower,
-        tokenize=copyseq_tokenize)
-
-    return fields
+'''

pykp/masked_loss.py
@@ -1,5 +1,8 @@
 import torch
+import math
+import logging
 
+EPS = 1e-8
 
 def masked_cross_entropy(class_dist, target, trg_mask, trg_lens=None,
                          coverage_attn=False, coverage=None, attn_dist=None, lambda_coverage=0):
@@ -16,7 +19,7 @@ def masked_cross_entropy(class_dist, target, trg_mask, trg_lens=None,
     """
     num_classes = class_dist.size(2)
     class_dist_flat = class_dist.view(-1, num_classes)  # [batch_size*trg_seq_len, num_classes]
-    log_dist_flat = torch.log(class_dist_flat)
+    log_dist_flat = torch.log(class_dist_flat + EPS)
     target_flat = target.view(-1, 1)  # [batch*trg_seq_len, 1]
     losses_flat = -torch.gather(log_dist_flat, dim=1, index=target_flat) # [batch * trg_seq_len, 1]
     losses = losses_flat.view(*target.size())  # [batch, trg_seq_len]
@@ -34,7 +37,13 @@ def masked_cross_entropy(class_dist, target, trg_mask, trg_lens=None,
         loss = losses.sum(dim=1) # [batch_size]
     '''
     loss = losses.sum(dim=1)  # [batch_size]
-    return loss.sum()
+    loss = loss.sum()
+
+    # Debug
+    #if math.isnan(loss.item()):
+    #    logging.info("NaN loss.")
+
+    return loss
 
 def compute_coverage_losses(coverage, attn_dist):
     """

train.py
@@ -14,7 +14,10 @@ from beam_search import SequenceGenerator
 from evaluate import evaluate_loss
 import train_ml
 from utils.statistics import Statistics
-from utils.visualization import plot_train_valid_curve
+from utils.report import export_train_and_valid_results
+from utils.time_log import time_since
+import time
+import math
 
 EPS = 1e-8
 
@@ -187,18 +190,17 @@ def train_model(model, optimizer_ml, optimizer_rl, criterion, train_data_loader,
     '''
     logging.info('======================  Start Training  =========================')
 
-    checkpoint_names = []
-    # best_loss = sys.float_info.max # for normal training/testing loss (likelihood)
-    best_loss = 0.0
-    stop_increasing = 0
     total_train_statistics = Statistics()
     report_train_statistics = Statistics()
     report_train_ppl = []
     report_valid_ppl = []
+    report_train_loss = []
+    report_valid_loss = []
 
     total_batch = -1
     early_stop_flag = False
 
+    best_valid_ppl = float('inf')
     best_valid_loss = float('inf')
     num_stop_dropping = 0
 
@@ -212,7 +214,7 @@ def train_model(model, optimizer_ml, optimizer_rl, criterion, train_data_loader,
         raise ValueError("Not implemented the function of load from trained model")
         pass
 
-    for epoch in range(opt.start_epoch, opt.epochs):
+    for epoch in range(opt.start_epoch, opt.epochs+1):
         if early_stop_flag:
             break
 
@@ -252,16 +254,37 @@ def train_model(model, optimizer_ml, optimizer_rl, criterion, train_data_loader,
             #progbar.update(epoch, batch_i, report_loss)
 
             # Checkpoint, decay the learning rate if validation loss stop dropping, apply early stopping if stop decreasing for several epochs.
-            if opt.train_ml:
-                if (opt.run_valid_every == -1 and batch_i == len(train_data_loader) - 1) or \
-                        (opt.run_valid_every > -1 and total_batch > 1 and total_batch % opt.run_valid_every == 0):
+            # Save the model parameters if the validation loss improved.
+            if opt.train_ml and epoch >= opt.start_checkpoint_at:
+                if (opt.checkpoint_interval == -1 and batch_i == len(train_data_loader) - 1) or \
+                        (opt.checkpoint_interval > -1 and total_batch > 1 and total_batch % opt.checkpoint_interval == 0):
                     # test the model on the validation dataset for one epoch
-                    eval_loss_stat = evaluate_loss(valid_data_loader, model, opt)
-                    current_eval_loss = eval_loss_stat.xent()
+                    valid_loss_stat = evaluate_loss(valid_data_loader, model, opt)
+                    current_valid_loss = valid_loss_stat.xent()
+                    current_valid_ppl = valid_loss_stat.ppl()
 
-                    if current_eval_loss < best_valid_loss:
-                        best_valid_loss = current_eval_loss
+                    current_train_ppl = report_train_statistics.ppl()
+                    current_train_loss = report_train_statistics.xent()
+
+                    # debug
+                    if math.isnan(current_valid_loss) or math.isnan(current_train_loss):
+                        logging.info(
+                            "NaN valid loss. Epoch: %d; batch_i: %d, total_batch: %d" % (epoch, batch_i, total_batch))
+                        exit()
+
+                    if current_valid_loss < best_valid_loss: # update the best valid loss and save the model parameters
+                        best_valid_loss = current_valid_loss
+                        best_valid_ppl = current_valid_ppl
                         num_stop_dropping = 0
+
+                        check_pt_model_path = os.path.join(opt.model_path, '%s.epoch=%d.batch=%d.total_batch=%d' % (
+                            opt.exp, epoch, batch_i, total_batch) + '.model')
+                        torch.save(  # save model parameters
+                            model.state_dict(),
+                            open(check_pt_model_path, 'wb')
+                        )
+                        logging.info('Saving checkpoint to %s' % check_pt_model_path)
+
                     else:
                         num_stop_dropping += 1
                         # decay the learning rate by a factor
@@ -271,27 +294,45 @@ def train_model(model, optimizer_ml, optimizer_rl, criterion, train_data_loader,
                             if old_lr - new_lr > EPS:
                                 param_group['lr'] = new_lr
 
-                    current_train_ppl = report_train_statistics.ppl()
-                    current_eval_ppl = eval_loss_stat.ppl()
-                    logging.info('Average training perplexity: %.3f; Average validation perplexity: %.3f; over %d iterations' % (current_train_ppl, current_eval_loss, opt.run_valid_every))
+                    # log loss, ppl, and time
+                    logging.info(
+                        '# batch: %d; average training perplexity: %.3f; average validation perplexity: %.3f; best validation perplexity: %.3f' % (
+                            total_batch, current_train_ppl, current_valid_ppl, best_valid_ppl))
+                    logging.info(
+                        '# batch: %d; average training loss: %.3f; average validation loss: %.3f; best validation loss: %.3f' % (
+                            total_batch, current_train_loss, current_valid_loss, best_valid_loss))
+                    train_forward_time, train_loss_compute_time, train_backward_time = report_train_statistics.total_time()
+                    valid_forward_time, valid_loss_compute_time, _ = valid_loss_stat.total_time()
+                    logging.info('# batch: %d; avg. training forward time: %.1f; avg. training loss compute time: %.1f; avg. training backward time: %.1f' % (
+                        total_batch, train_forward_time, train_loss_compute_time, train_backward_time
+                    ))
+                    logging.info('# batch: %d; avg. validation forward time: %.1f; avg. validation loss compute time: %.1f' % (
+                            total_batch, valid_forward_time, valid_loss_compute_time
+                    ))
 
                     report_train_ppl.append(current_train_ppl)
-                    report_valid_ppl.append(current_eval_ppl)
+                    report_valid_ppl.append(current_valid_ppl)
+                    report_train_loss.append(current_train_loss)
+                    report_valid_loss.append(current_valid_loss)
 
                     if num_stop_dropping >= opt.early_stop_tolerance:
                         logging.info('Have not increased for %d epochs, early stop training' % num_stop_dropping)
                         early_stop_flag = True
                         break
 
-                    report_train_statistics.reset()
+                    report_train_statistics.clear()
 
-    train_valid_curve_path = opt.exp_path + '/train_valid_ppl.pdf'
-    plot_train_valid_curve(report_train_ppl, report_valid_ppl, opt.run_valid_every, train_valid_curve_path)
+    # export the training curve
+    train_valid_curve_path = opt.exp_path + '/train_valid_curve'
+    export_train_and_valid_results(report_train_loss, report_valid_loss, report_train_ppl, report_valid_ppl, opt.checkpoint_interval, train_valid_curve_path)
     logging.info('Overall average training loss: %.3f, ppl: %.3f' % (total_train_statistics.xent(), total_train_statistics.ppl()))
 
 def main(opt):
     try:
+        start_time = time.time()
         train_data_loader, valid_data_loader, word2idx, idx2word, vocab = load_data_vocab(opt)
+        load_data_time = time_since(start_time)
+        logging.info('Time for loading the data: %.1f' % load_data_time)
         model = init_model(opt)
         optimizer_ml, optimizer_rl, criterion = init_optimizer_criterion(model, opt)
         train_model(model, optimizer_ml, optimizer_rl, criterion, train_data_loader, valid_data_loader, opt)

train_ml.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 from pykp.masked_loss import masked_cross_entropy
 from utils.statistics import Statistics
+from utils.time_log import time_since
+import time
 
 def train_one_batch(one2one_batch, model, optimizer, opt):
 
@@ -31,16 +33,18 @@ def train_one_batch(one2one_batch, model, optimizer, opt):
 
     optimizer.zero_grad()
 
+    start_time = time.time()
     decoder_dist, h_t, attention_dist, coverage = model(src, src_lens, trg, src_oov, max_num_oov, src_mask)
+    forward_time = time_since(start_time)
 
-    #start_time = time.time()
-
+    start_time = time.time()
     if opt.copy_attention:  # Compute the loss using target with oov words
         loss = masked_cross_entropy(decoder_dist, trg_oov, trg_mask, trg_lens,
                          opt.coverage_attn, coverage, attention_dist, opt.lambda_coverage)
     else:  # Compute the loss using target without oov words
         loss = masked_cross_entropy(decoder_dist, trg, trg_mask, trg_lens,
                                     opt.coverage_attn, coverage, attention_dist, opt.lambda_coverage)
+    loss_compute_time = time_since(start_time)
 
     total_trg_tokens = sum(trg_lens)
 
@@ -49,8 +53,12 @@ def train_one_batch(one2one_batch, model, optimizer, opt):
     else: # use batch_size to normalize the loss
         normalization = src.size(0)
 
+    assert normalization > 0, 'normalization should be a positive number'
+
+    start_time = time.time()
     # back propagation on the normalized loss
     loss.div(normalization).backward()
+    backward_time = time_since(start_time)
 
     if opt.max_grad_norm > 0:
         grad_norm_before_clipping = nn.utils.clip_grad_norm_(model.parameters(), opt.max_grad_norm)
@@ -60,6 +68,6 @@ def train_one_batch(one2one_batch, model, optimizer, opt):
     optimizer.step()
 
     # construct a statistic object for the loss
-    stat = Statistics(loss.item(), total_trg_tokens)
+    stat = Statistics(loss.item(), total_trg_tokens, n_batch=1, forward_time=forward_time, loss_compute_time=loss_compute_time, backward_time=backward_time)
 
     return stat, decoder_dist.detach()